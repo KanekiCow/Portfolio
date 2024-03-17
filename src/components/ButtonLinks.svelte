a145211260d77aa122d787c2028d455d6c2ec391
Kaneku cat <tamkukumanu@yahoo.com>
2024-03-17 17:15:16 +0700

    improvements on the buttons of the projects

--- src/components/ButtonLinks.svelte
+++ src/components/ButtonLinks.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  export let links: { label: string; url: string; }[] = [];
+  export let links: { label: string; url: string }[] = [];
 
   function handleClick(url: string) {
     window.open(url, "_blank");
@@ -8,6 +8,10 @@
 
 <div class="flex flex-row space-x-3 mt-5">
   {#each links as link}
-    <button on:click={() => handleClick(link.url)} class="h-[34px] item p-[5px] px-[10px] rounded-lg active:scale-[.85] ease-linear transition-all duration-100 flex flex-row justify-center items-center bg-black/20">{link.label}</button>
+    <button
+      on:click={() => handleClick(link.url)}
+      class="h-[34px] item p-[5px] px-[10px] rounded-lg active:scale-[.85] ease-linear transition-all duration-100 flex flex-row justify-center items-center hover:bg-gray-500/20 bg-black/20 border-[1px] border-gray-600/40"
+      >{link.label}</button
+    >
   {/each}
 </div>
